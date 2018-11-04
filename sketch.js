10bdcb6862936a8e6e3cd7e45cf75860ee779359
Alec <afroenygam@gmail.com>
2018-11-04 18:20:09 -0500

    project initialized, main function updated

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var tree
+
+function setup() {
+    noCanvas() // Stops P5 program from requiring a canvas
+    tree = new Tree() // Creates a tree object to hold our data
+    for (var i=0; i < 10; i++) // Adds 10 random numbers between 0 and 100 inclusive(?) to BST to be traversed
+    {
+	tree.addValue(floor(random(0,100))
+    }
+    console.log(tree)
+    tree.traverse() // Traverses and prints out the path
+
+    var result = tree.search(10) // Searches for the given number in the tree. Stores in var result if found
+    if (result == null)
+    {
+	console.log("Not found")
+    }
+    else
+    {
+	console.log(result)
+    }
+}
